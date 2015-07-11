7559d1fced2fe551578b0139173ff55261d66242
Dan Fu <dan@danscode.com>
2015-07-11 13:07:02 -0400

    Fix some merge conflicts, add header

---
+++ app/www/js/views/header.js
@@ -0,0 +1,21 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+
+    'text!templates/header.ejs'
+], function($, _, Backbone,
+        headerTemplate
+) {
+    var headerView = Backbone.View.extend({
+        template: _.template(headerTemplate),
+
+        render: function() {
+            var self = this;
+
+            this.$el = this.template;
+        }
+    });
+
+    return headerView;
+});
